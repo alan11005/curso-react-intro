308f7794d68e11bd5160d993aabcf7773984fc21
alan11005 <alagarciasa@unal.edu.co>
2023-09-03 12:31:54 -0500

    onClick-onChange

--- src/ToDoButton.jsx
+++ src/ToDoButton.jsx
@@ -2,7 +2,13 @@ import './ToDoButton.css';
 
 function ToDoButton(){
     return(
-        <button className='CreateTodoButton'>+</button>
+        <button 
+        className='CreateTodoButton'
+        onClick={(event)=>{
+            console.log(event)
+            console.log(event.target)
+        }}
+        >+</button>
     )
 }
 
--- src/ToDoInput.jsx
+++ src/ToDoInput.jsx
@@ -1,10 +1,20 @@
+import React from 'react';
 import './ToDoInput.css';
 
 function ToDoInput(){
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log('Los usuarios buscan: '+ searchValue)
+
     return(
         <input 
         placeholder="Aqui"
         className='TodoSearch'
+        value = {searchValue}
+        onChange={(event) => {
+            setSearchValue(event.target.value)
+            
+        }}
         />
     );
 }
